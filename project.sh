c5618beb8c56c4b1869cabb9a977d2f955e08be8
Dipan Sunar <160516349+Atreya556@users.noreply.github.com>
2026-03-13 01:39:45 +0200

    Add files via upload

---
+++ project.sh
@@ -0,0 +1,49 @@
+(brijesh㉿kali)-[~]
+└─$ cd ~/Desktop    
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop]
+└─$ ls
+FileProject
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop]
+└─$ cd FileProject
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop/FileProject]
+└─$ ls
+Images  TextFiles  auto_move.sh
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop/FileProject]
+└─$ ls Images
+brijesh.jpg  photo.jpg  pic1.jpg  ranjit.jpg
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop/FileProject]
+└─$ ls TextFiles
+brijesh.txt  file1.txt  notes.txt  ranjit.txt
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop/FileProject]
+└─$ chmod +x auto_move.sh
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop/FileProject]
+└─$ ./auto_move.sh
+Files moved successfully!
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop/FileProject]
+└─$ nano ./auto_move.sh 
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop/FileProject]
+└─$ touch deepan.jpg
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop/FileProject]
+└─$ touch deepan.txt
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop/FileProject]
+└─$ chmod +x auto_move.sh
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop/FileProject]
+└─$ ./auto_move.sh
+Files moved successfully!
+                                                                                                                   
+┌──(brijesh㉿kali)-[~/Desktop/FileProject]
+└─$ ls TextFiles
+brijesh.txt  deepan.txt  file1.txt  notes.txt  ranjit.txt
+                                                            
